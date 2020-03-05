e5166d96d7d3c3af53fbccc9432efdf02cb4c7d5
MOKHTAR <39674365+mohamed-mokhtar@users.noreply.github.com>
2020-03-05 12:53:23 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
-import pylab
 import imageio
 import os 
 import numpy as np
-import cv2 as cv
+#import cv2 as cv
 #import skimage as sk 
 
 def video_reader(path='simb.mp4'):
@@ -18,6 +17,48 @@ def video_writer(vid):
     writer.close()
     print(vid.get_length())
     print(np.array(vid.get_data(10)).shape)
+
+def withdraw(balance, lock):     
+    for _ in range(10000): 
+        lock.acquire() 
+        balance.value = balance.value - 1
+        lock.release() 
+  
+# function to deposit to account 
+def deposit(balance, lock):     
+    for _ in range(10000): 
+        lock.acquire() 
+        balance.value = balance.value + 1
+        lock.release() 
+  
+def perform_transactions(): 
+  
+    # initial balance (in shared memory) 
+    balance = multiprocessing.Value('i', 100) 
+
+    # creating a lock object 
+    lock = multiprocessing.Lock() 
+
+    # creating new processes 
+    p1 = multiprocessing.Process(target=withdraw, args=(balance,lock)) 
+    p2 = multiprocessing.Process(target=deposit, args=(balance,lock)) 
+  
+    # starting processes 
+    p1.start() 
+    p2.start() 
+  
+    # wait until processes are finished 
+    p1.join() 
+    p2.join() 
+  
+    # print final balance 
+    print("Final balance = {}".format(balance.value)) 
+    # initial balance (in shared memory) 
+    balance = multiprocessing.Value('i', 100) 
+
+    # creating a lock object 
+    lock = multiprocessing.Lock() 
+
 
 if (__name__ == '__main__'):
     print("Hello Utils")
