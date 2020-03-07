e2f4944ee7015fbbb094348f4840f21f12ef6ae2
Sofyan Mahmoud <sofyan1020@gmail.com>
2020-03-07 14:51:46 +0200

    adding successful and replicate

--- testreciever.py
+++ testreciever.py
@@ -11,20 +11,25 @@ import signal
 import threading
 
 
-# def saveVideo(video,videoName:str):    
-#     try:
-#         with open(videoName,'wb') as myfile:
-#             myfile.write(video)
-#         return True
-#     except:
-#         return False
+def saveVideo(video,videoName:str):    
+    try:
+        with open(videoName,'wb') as myfile:
+            myfile.write(video)
+        return True
+    except:
+        return False
 
+DataOfVid = {"VIDEO_NAME":"cat.mp4"}
 context = zmq.Context()
-reciever = context.socket(zmq.PULL) 
-reciever.bind("tcp://192.168.0.103:21212")
-while True:
-    message = reciever.recv_pyobj()
-    print(message)
+reciever = context.socket(zmq.REQ) 
+reciever.connect("tcp://127.0.1.1:21211")
+reciever.send_pyobj(DataOfVid)
+video=reciever.recv_pyobj()
+# print(video)
+if(saveVideo(video,"Downloaded"+".mp4")):
+    print("The file has been saved")
+else : 
+    print("The file hasn't been saved")
 
 
 
